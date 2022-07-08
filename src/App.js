2010a66aca8d2047ca102f425f2efaab618ea81f
k.sai@p3fusion.com <k.sai@p3fusion.com>
2022-07-08 21:23:03 +0530

    .

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React updated
+          Sai Demo Application
         </a>
       </header>
     </div>
